8e3b7e6c7ce87cf20dd584cbec34cf180fc21b35
Dinesh Dhanji <56204876+DineshDhanji@users.noreply.github.com>
2020-09-12 17:26:25 +0500

    Add files via upload

---
+++ Rough Work-Animeia/index.js
@@ -0,0 +1,11 @@
+function navFunc()
+{
+    document.getElementsByClassName('navbar')[0].classList.toggle("navbar-show")
+    document.getElementsByClassName('navbar')[0].classList.toggle("navbar-show-2")
+}
+
+
+function myAlert()
+{
+    alert("Don't use your personal information here it just a assignment")
+}
